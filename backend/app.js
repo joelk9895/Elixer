6fa476ef5c4ac6ee4c898bd89aaa0447096a6c10
sanjay-sudheer <sanjaysudheer2003@gmail.com>
2024-02-25 03:18:11 +0530

    post req

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const { run } = require('node:test');
 const uri = "mongodb+srv://joelk9895:qysguc-tuJpat-2woctu@opinio.qkbdnis.mongodb.net/?retryWrites=true&w=majority&appName=opinio";
 const client = new MongoClient(uri);
 const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
@@ -31,6 +35,24 @@ app.get('/consult', (req, res) => {
         const database = client.db('elixer');
         const collection = database.collection('consult');
         const user = await collection.find({}).toArray();
+        console.log(user);
+        res.send(user);
+        } finally {
+        await client.close();
+        }
+    }
+    run().catch(console.dir);
+});
+
+app.post('/details', (req, res) => {
+    const data = req.body.id;
+    async function run() {
+        try {   
+        await client.connect();
+        const database = client.db('elixer');
+        const collection = database.collection('consult');
+        const query = { _id: new ObjectId(data) };
+        const user = await collection.findOne(query);
         console.log(user);
         res.send(user);
         } finally {
